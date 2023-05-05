444a0254b341df3af00e1996d883212156aca34c
Wizzygurl <udemezuewisdom7@gmailcom>
2023-05-05 08:55:54 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,9 +9,9 @@ int *p;
 a[2] = 1024;
 p = &n;
   /*
-   * Description - add line to code
    * write your line of code here...
    * Remember:
+   * main - magic
    * - you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement
